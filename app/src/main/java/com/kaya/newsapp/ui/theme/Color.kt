7e14fcbef2de0df36a74e4acd2857dc501c67784
Hasan Yiğit Kaya <68725704+yigtkaya@users.noreply.github.com>
2023-08-25 09:08:33 +0300

    colors added

--- app/src/main/java/com/kaya/newsapp/ui/theme/Color.kt
+++ app/src/main/java/com/kaya/newsapp/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.kaya.newsapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val paleRed = Color(0xFFFFE8E5)
+val paleYellow = Color(0xFFFFF2C5)
+val paleBlue = Color(0xFFE0F1FF)
+val paleGreen = Color(0xFFE5F8E5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)

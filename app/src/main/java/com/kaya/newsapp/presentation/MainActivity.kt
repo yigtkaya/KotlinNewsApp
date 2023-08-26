4df59ba75d4884825c3e20de00f099e20e7635dd
Hasan Yiğit Kaya <68725704+yigtkaya@users.noreply.github.com>
2023-08-26 11:28:24 +0300

    floating action button as nav bar

--- app/src/main/java/com/kaya/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/kaya/newsapp/presentation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -42,7 +43,8 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = Color.Black) {
                     Scaffold (
-                        bottomBar = {
+                        floatingActionButtonPosition = FabPosition.Center,
+                        floatingActionButton = {
                             CustomBottomNavBar(items = bottomList, navController = navController)
                         },
                         topBar = { ToolBar() },
--- app/src/main/java/com/kaya/newsapp/presentation/bottomNavBar/CustomBottomNavBar.kt
+++ app/src/main/java/com/kaya/newsapp/presentation/bottomNavBar/CustomBottomNavBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -16,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
@@ -34,9 +36,9 @@ fun CustomBottomNavBar(
 
     Row (
         modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Color.Black)
-            .clip(shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+            .clip(CircleShape)
+        .background(color = Color.Black)
+            .shadow(elevation = 16.dp, shape = CircleShape),
         verticalAlignment = Alignment.Bottom,
         horizontalArrangement = Arrangement.SpaceEvenly,
     ){
@@ -68,22 +70,13 @@ fun CustomNavBarItem(
     isSelected: Boolean,
     onClick : () -> Unit,
 ) {
-    val backgroundColor = if (isSelected) Color.White else Color.Black
 
     Box(modifier = Modifier
+        .padding(2.dp)
         .clip(CircleShape)
-        .padding(top = 16.dp)
-        .background(backgroundColor)
-        .clickable(onClick = onClick)) {
-        Row(
-            modifier = Modifier
-                .padding(12.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-
-        ) {
-                    Icon(imageVector = item.icon, contentDescription = null, tint = if (isSelected) Color.Blue else Color.White)
-            }
-        }
+        .background(color = if (isSelected) Color.White else Color.Black)
+        .clickable(onClick = onClick).padding(4.dp)) {
+        Icon(imageVector = item.icon, contentDescription = null, tint = if (isSelected) Color.Black else Color.White, modifier = Modifier.size(32.dp))
+    }
     }
 
